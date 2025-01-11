f85c48deb80498cab8f6d1ee080de917a33fdac1
Karthikraut <karthikraut2@gmail.com>
2025-01-11 21:19:55 +0530

    Register url has been created

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express  =require("express")
-const {connectMongoose} =require("./database.js")
+const {connectMongoose,User} =require("./database.js")
 const app =express()
 
 connectMongoose();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.get("/",(req,res)=>{
     res.send("Hello, Karthik today we are learning passport.js")
 })
 
-app.post("/register",(req,res)=>{
+app.post("/register",async (req,res)=>{
+    const user = await User.findOne({username:req.body.username});
 
+    if(user) return res.status(400).send("User already exists");
+
+    const newUser =await User.create(req.body);
+
+    res.status(201).send(newUser);
 })
 
 app.listen(3000,()=>{
