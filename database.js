b74144b0b96d02cde96daee4b72136f4419911a0
Karthikraut <karthikraut2@gmail.com>
2025-01-11 21:14:36 +0530

    User schema for mongoose has been prepared

--- database.js
+++ database.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 exports.connectMongoose = ()=>{
     mongoose.connect("mongodb://localhost:27017/passport").then((e)=>{
         console.log(`Connected to Mongodb: ${e.connection.host}`)
     }).catch((e)=>console.log(e));
 };
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type:String,
+        required: true,
+        unique: true,
+    },
+    password: String,
+})
+
+exports.User = mongoose.model("User", userSchema);
