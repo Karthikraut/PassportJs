84d51d7ccbc70be4cd9bbc69c129dbeafc453ace
Karthikraut <karthikraut2@gmail.com>
2025-01-11 21:12:07 +0530

    MongoDB has been initialised

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express  =require("express")
+const {connectMongoose} =require("./database.js")
 const app =express()
 
+connectMongoose();
+app.get("/",(req,res)=>{
+    res.send("Hello, Karthik today we are learning passport.js")
+})
+
+app.post("/register",(req,res)=>{
+
+})
 
 app.listen(3000,()=>{
     console.log("Server is Running on Port:3000");
